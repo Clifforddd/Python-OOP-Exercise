ba87269e39cf52a9ec13ef28294010be6affc94d
Clifforddd <90348904+Clifforddd@users.noreply.github.com>
2022-07-11 15:12:36 -0700

    Update wordfinder.py

--- wordfinder.py
+++ wordfinder.py
@@ -1,1 +1,31 @@
+"""Word Finder: finds random words from a dictionary."""
+import random
 
+class WordFinder:
+    """To return random words"""
+
+
+    def __init__(self, path):
+
+        dict_file = open(path)
+
+        self.words = self.parse(dict_file)
+
+        print(f"{len(self.words)} words read")
+
+    def parse(self, dict_file):
+
+        return [w.strip() for w in dict_file]
+
+    def random(self):
+
+        return random.choice(self.words)
+
+    
+class SpecialWF(WordFinder):
+    """Read special character"""
+
+    def parse(self, dict_file):
+
+        return [i.strip() for i in dict_file
+                if i.strip() and not i.startswith("#")]
